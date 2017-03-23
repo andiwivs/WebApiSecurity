d27a4cc2ad84efb6ff1682b3384b690386d8ef50
Andrew Withers <andreww@thomasdev.co.uk>
2017-03-23 12:11:17 +0000

    Add empty web project with simple http module and helper for debug output

---
+++ SecurityPipeline/Pipeline/HttpModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+
+namespace SecurityPipeline
+{
+    public class HttpModule : IHttpModule
+    {
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            Helper.Write("HttpModule", HttpContext.Current.User);
+        }
+
+        public void Dispose()
+        {}
+    }
+}
