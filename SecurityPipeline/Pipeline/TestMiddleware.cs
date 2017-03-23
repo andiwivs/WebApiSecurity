c3478fc274d6636dcc5405c170423c2e40b353bb
Andrew Withers <andreww@thomasdev.co.uk>
2017-03-23 14:32:29 +0000

    Set fake user principal in custom middleware

--- SecurityPipeline/Pipeline/TestMiddleware.cs
+++ SecurityPipeline/Pipeline/TestMiddleware.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Owin;
 using System;
 using System.Collections.Generic;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace SecurityPipeline.Pipeline
@@ -18,6 +19,9 @@ namespace SecurityPipeline.Pipeline
         public async Task Invoke(IDictionary<string, object> env)
         {
             var context = new OwinContext(env); // fancy wrapper around the dictionary, useful for working with preset keys for things like user context
+
+            // TODO: implement proper authenticate logic / plumbing here
+            context.Request.User = new GenericPrincipal(new GenericIdentity("anders"), new string[0]);
 
             Helper.Write("Middleware", context.Request.User);
 
