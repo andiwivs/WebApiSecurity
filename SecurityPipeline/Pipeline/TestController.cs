e76ff56262b8a9a0cba0fc09208c20db781f9662
Andrew Withers <andreww@thomasdev.co.uk>
2017-03-23 12:41:27 +0000

    Add web api and owin nuget packages with test controller hooked into debug output

---
+++ SecurityPipeline/Pipeline/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace SecurityPipeline.Pipeline
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            Helper.Write("Controller", User);
+
+            return Ok();
+        }
+    }
+}
---
+++ SecurityPipeline/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace SecurityPipeline
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                "default",
+                "api/{controller}"
+            );
+
+            app.UseWebApi(config);
+        }
+    }
+}
